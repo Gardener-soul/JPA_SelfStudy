d6e746b742e002b5f71afe18c251b743499a9a99
Gardener-Soul <str-pes@naver.com>
2023-12-14 15:49:26 +0900

    Upload 양방향 매핑관계
    수업 Section 5 양방향 매핑관계를 수강한 후 업로드
    
    Related Issue no : none

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -2,6 +2,7 @@ package jpabook.jpashop;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -16,17 +17,15 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Order order = new Order();
+            em.persist(order);
 
-            Order order = em.find(Order.class, 1L);
-            Long memberId = order.getMemberId();
+//            order.addOrderItem(new OrderItem());
+            // 위와 아래와 동일
+            OrderItem orderItem = new OrderItem();
+            orderItem.setOrder(order);
+            em.persist(orderItem);
 
-            Member member = em.find(Member.class, memberId);
-            // 위는 객체지향적이지 않음,,,
-
-            // Order에 바로 Member 클래스 주입을 통해, order에서 Member로 바로 접근 가능.
-            Member findMember = order.getMember();
-
-            tx.commit();
         } catch (Exception e){
             tx.rollback();
         } finally {
